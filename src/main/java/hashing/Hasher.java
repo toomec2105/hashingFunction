65ad577786daa1f52466b62d0fb50a3fe86901d0
toomec2105 <tomec2105@interia.pl>
2020-05-17 16:27:50 +0200

    add HashMap

--- src/main/java/hashing/Hasher.java
+++ src/main/java/hashing/Hasher.java
@@ -3,17 +3,16 @@ package hashing;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.HashMap;
 
 public class Hasher {
-	
-	
+	private final String[] letters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+			"q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
 
 	public int hashUsingArrayList(String input) {
 		if (input == null) {
 			return -1;
 		}
-		String[] letters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
-				"s", "t", "u", "v", "w", "x", "y", "z" };
 		List<String> alphabet = new ArrayList<String>(Arrays.asList(letters));
 		int hashCode = 0;
 
@@ -24,6 +23,30 @@ public class Hasher {
 				hashCode += alphabet.indexOf(letter);
 			}
 		}
+		return hashCode;
+	}
+
+	public int hashUsingHashMap(String input) {
+		if (input == null) {
+			return -1;
+		}
+
+		HashMap<String, Integer> alphabet = new HashMap<String, Integer>();
+		for (int i = 0; i < letters.length; i++) {
+			alphabet.put(letters[i], i);
+		}
+
+		int hashCode = 0;
+
+		for (int i = 0; i < input.length(); i++) {
+			String letter = String.valueOf(input.charAt(i));
+			Integer hash = alphabet.get(letter);
+
+			if (null != hash) {
+				hashCode += hash;
+			}
+		}
+
 		return hashCode;
 	}
 }
